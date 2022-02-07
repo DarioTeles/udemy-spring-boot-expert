350a948dad1b3e38379f37c272e765773dd0e24e
Dário Teles <dariotelesdeoliveirasocial@hotmail.com>
2022-02-07 11:31:16 -0300

    Delete Seção 7 - Documentação de API com SWAGGER/vendas directory

--- Seção 7 - Documentação de API com SWAGGER/vendas/src/main/java/io/github/darioteles/domain/enums/StatusPedido.java
+++
@@ -1,9 +0,0 @@
-package io.github.darioteles.domain.enums;
-
-/**
- * Define uma classe Enum que define os status de um pedido.
- */
-public enum StatusPedido {
-    REALIZADO,
-    CANCELADO;
-}
--- Seção 7 - Documentação de API com SWAGGER/vendas/src/main/java/io/github/darioteles/exception/SenhaInválidaExpection.java
+++
@@ -1,7 +0,0 @@
-package io.github.darioteles.exception;
-
-public class SenhaInválidaExpection extends RuntimeException {
-    public SenhaInválidaExpection() {
-        super("Senha inválida");
-    }
-}
